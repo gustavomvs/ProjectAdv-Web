15dd502d745c6cf75fd89ed5015a4e40a3278f22
Gustavo Moura <g.gustavosouma@gmail.com>
2024-11-03 21:46:20 -0300

    .

--- src/Login.tsx
+++ src/Login.tsx
@@ -2,6 +2,7 @@ const CLIENT_ID = "1000.WTU36HCZ6AMQN1HGW83TY4IE0NFOSI";
 const REDIRECT_URI =
   "https://4af0-2804-56c-d746-a700-fdff-8dd7-4c0-bfee.ngrok-free.app/callback"; // Redireciona para o backend
 
+
 const Login = () => {
   const handleLogin = () => {
     const authUrl = `https://accounts.zoho.com/oauth/v2/auth?response_type=code&client_id=${CLIENT_ID}&scope=ZohoBigin.modules.ALL&access_type=offline&redirect_uri=${encodeURIComponent(
